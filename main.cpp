aebf1c887ec5056e1f99c534b797ada9cc6f8105
Megan <mko5258@psu.edu>
2023-10-05 01:41:33 -0400

    Created functions

--- main.cpp
+++ main.cpp
@@ -1,2 +1,30 @@
+#include <iostream>
+#include <vector>
 
+
+using namespace std;
+
+// AERSP 424 Homework 1: Question 5
+// Goal: Write functions named sigmoid and gradient_sigmoid that
+// compute equations.
+
+double sigmoid(double); //function prototype for sigmoid
+double gradient_sigmoid(double);//function prototype for gradient sigmoid
+
+double z = 0.0176835; //given value for z
+double sigma = 0; //initialize variable sigma
+double sigmaprime = 0; //initialize variable sigmaprime
+
+
+double sigmoid(double z) //sigmoid function
+{
+	sigma = 1 / (1 + exp(-z)); //function given in doc
+	return sigma; //return value calculated for sigma
+}
+
+double gradient_sigmoid(double sigma) //gradient_sigmoid function
+{
+	sigmaprime = sigma * (1 - sigma); //function given in doc
+	return sigmaprime; //return value calculated for sigmaprime
+}
 
